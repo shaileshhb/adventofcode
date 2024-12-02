c0b24fb741e42222a6149f6783093a831082fc24
shaileshhb <shaileshb.0720@gmail.com>
2024-12-02 22:40:49 +0530

    incorrect code added

--- 2024/day2/rednosereport.go
+++ 2024/day2/rednosereport.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -12,10 +13,13 @@ func main() {
 	input := file.ReadFromFile()
 
 	reports := processFileInput(input)
-	fmt.Println(reports)
+	// fmt.Println(reports)
+
+	removeArrayWithDuplicates(reports)
 
 	safeReports := getSafeReportCount(reports)
 	fmt.Println("safe reports: ", safeReports)
+	// test()
 }
 
 func processFileInput(input [][]string) [][]int {
@@ -43,20 +47,94 @@ func getSafeReportCount(reports [][]int) int {
 	safeReports := 0
 
 	for _, report := range reports {
-		isIncreasing := false
-		previousNum := report[0]
-		for i, num := range report {
-			if i == 0 {
-				continue
-			}
+		isIncreasing := report[0] < report[1]
+		isSafe := true
+		for i := 1; i < len(report)-1; i++ {
 
-			if isIncreasing && previousNum <= num {
-				isIncreasing = false
+			if isIncreasing {
+				if report[i] < report[i+1] && (report[i+1]-report[i] >= 1) && (report[i+1]-report[i] <= 3) {
+					continue
+				}
+				isSafe = false
 				break
 			}
 
+			if report[i] > report[i+1] && (report[i]-report[i+1] >= 1) && (report[i]-report[i+1] <= 3) {
+				continue
+			}
+
+			isSafe = false
+			break
+		}
+
+		fmt.Println("reports", report, isSafe)
+		if isSafe {
+			safeReports++
 		}
 	}
 
 	return safeReports
+}
+
+func removeArrayWithDuplicates(reports [][]int) {
+	newReports := make([][]int, 0)
+
+	for _, report := range reports {
+		tempReport := make([]int, len(report))
+		copy(tempReport, report)
+
+		sort.Ints(tempReport)
+
+		isDuplicateFound := false
+		for i := 1; i < len(tempReport)-1; i++ {
+			if tempReport[i] == tempReport[i+1] {
+				isDuplicateFound = true
+				break
+			}
+		}
+
+		fmt.Println("== duplicate rports", tempReport, isDuplicateFound)
+
+		if !isDuplicateFound {
+			newReports = append(newReports, report)
+		}
+	}
+
+	fmt.Println("len", len(newReports), len(reports))
+}
+
+func test() {
+	// report := []int{7, 6, 4, 2, 1}
+	report := []int{17, 18, 22}
+	safeReports := 0
+
+	isIncreasing := report[0] < report[1]
+	isSafe := true
+	fmt.Println("isIncreasing", isIncreasing)
+
+	for i := 1; i < len(report)-1; i++ {
+		fmt.Println("value", report[i], report[i+1])
+
+		if isIncreasing {
+			if report[i] < report[i+1] && (report[i+1]-report[i] >= 1) && (report[i+1]-report[i] <= 3) {
+				continue
+			}
+			isSafe = false
+			fmt.Println("this has to break", i, report[i])
+			break
+		}
+
+		if report[i] > report[i+1] && (report[i]-report[i+1] >= 1) && (report[i]-report[i+1] <= 3) {
+			continue
+		}
+
+		isSafe = false
+		fmt.Println("breaking for i", i)
+		break
+	}
+
+	if isSafe {
+		safeReports++
+	}
+	fmt.Println("safe reports", safeReports)
 }
